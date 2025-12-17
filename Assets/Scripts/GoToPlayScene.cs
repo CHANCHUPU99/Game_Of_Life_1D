02f9bbfadd234195973afb3db96564dc28ee1779
CHANCHUPU99 <106758212+CHANCHUPU99@users.noreply.github.com>
2025-12-17 10:59:12 -0600

    version pre build
    checar funcionamiento visual en celular y ya

---
+++ Assets/Scripts/GoToPlayScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToPlayScene : MonoBehaviour
+{
+    public void goToPlayScene() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void exitGame() {
+        Application.Quit();
+    }
+
+    public void goToMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+}
